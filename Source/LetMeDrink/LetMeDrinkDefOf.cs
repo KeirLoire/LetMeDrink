a4ca42ac85ee788d5aad6720da29b89b955688bc
Chester J. Ayala <chesterayala.ca@gmail.com>
2024-07-11 19:50:12 +0800

    Add Dub's Bad Hygiene thirst requirement

--- Source/LetMeDrink/LetMeDrinkDefOf.cs
+++ Source/LetMeDrink/LetMeDrinkDefOf.cs
@@ -6,8 +6,11 @@ namespace LetMeDrink
     [DefOf]
     public static class LetMeDrinkDefOf
     {
+        [MayRequire("bhdlitemodestub")]
         public static JobDef AnimalDrinkFromBasin;
+        [MayRequire("bhdlitemodestub")]
         public static ThingDef PetWaterBowl;
+        [MayRequire("bhdlitemodestub")]
         public static ThingDef WaterTrough;
 
         static LetMeDrinkDefOf()
